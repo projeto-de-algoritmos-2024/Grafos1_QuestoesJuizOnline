0501ada8e25245f56a1cebaf06d5b797a546c610
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-04-13 20:00:28 -0300

    Versão final do exercício movimentos do Cavalo

--- Movimentos do Cavalo/movimentos_cavalo.c
+++ Movimentos do Cavalo/movimentos_cavalo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define SIZE 8
 
-typedef struct{
+typedef struct {
     int row;
     int col;
 } Position;
@@ -13,25 +13,43 @@ int isValidPosition(int row, int col) {
     return row >= 0 && row < SIZE && col >= 0 && col < SIZE;
 }
 
-int knightMovies(Position start, Position end){
-    // Movimentações possíveis do cavalo
-    int d_row[] = {-2, -1, 1, 2, 2, 1, -1, -2};
-    int d_col[] = { 1,  2, 2, 1,-1,-2, -2, -1};
-
+int knightMoves(Position start, Position end) {
+    // Posições do Cavalo em qualquer direção
+    int dr[] = {-2, -1, 1, 2, 2, 1, -1, -2};
+    int dc[] = {1, 2, 2, 1, -1, -2, -2, -1};
+    // Matriz de movimentos
     int dist[SIZE][SIZE];
     memset(dist, -1, sizeof(dist));
-
-    //Fila da BFS
+    // Fila
     Position queue[SIZE * SIZE];
     int front = 0, rear = 0;
-
-    //Posição inicial
-
-    // Percurso em BFS
-    
+    queue[rear++] = start;
+    dist[start.row][start.col] = 0;
+    // BFS
+    while (front < rear) {
+        Position current = queue[front++];
+        if (current.row == end.row && current.col == end.col) {
+            return dist[current.row][current.col];
+        }
+        for (int i = 0; i < 8; ++i) {
+            int newRow = current.row + dr[i];
+            int newCol = current.col + dc[i];
+            if (isValidPosition(newRow, newCol) && dist[newRow][newCol] == -1) {
+                queue[rear++] = (Position){newRow, newCol};
+                dist[newRow][newCol] = dist[current.row][current.col] + 1;
+            }
+        }
+    }
+    return -1;
 }
 
-int main(){
-
+int main() {
+    char a[3], b[3];
+    while (scanf("%s %s", a, b) != EOF) {
+        Position start = {(int)(a[1] - '1'), (int)(a[0] - 'a')};
+        Position end = {(int)(b[1] - '1'), (int)(b[0] - 'a')};
+        int moves = knightMoves(start, end);
+        printf("To get from %s to %s takes %d knight moves.\n", a, b, moves);
+    }
     return 0;
 }
